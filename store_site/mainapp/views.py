4eee17313d37f39f771947be42ebff8795a47df7
e.murij <murij.kat@gmail.com>
2021-07-19 13:54:54 +0700

    hw_2

--- store_site/mainapp/views.py
+++ store_site/mainapp/views.py
@@ -6,7 +6,7 @@ def index(request):
     context = {
         'title': title,
     }
-    print(context)
+    print(request.resolver_match.url_name)
     return render(request, 'mainapp/index.html', context)
 
 
@@ -21,7 +21,15 @@ def contact(request):
 
 def product(request):
     title = 'Каталог'
+    links_menu = [
+        {'href': 'product', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'},
+    ]
     context = {
         'title': title,
+        'links_menu': links_menu,
     }
     return render(request, 'mainapp/products.html', context)
