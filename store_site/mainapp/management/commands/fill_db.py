5dfe4af7f0475c77da07f6251c437f2204267bab
e.murij <murij.kat@gmail.com>
2021-07-27 12:44:48 +0700

    lesson_4

---
+++ store_site/mainapp/management/commands/fill_db.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+import json
+import os
+from mainapp.models import ProductCategory
+from mainapp.models import Product
+
+
+JSON_PATH = 'mainapp/jsons'
+
+
+def load_from_json(file_name):
+    with open(os.path.join(JSON_PATH, file_name + '.json'), 'r', encoding='UTF-8') as file:
+        return json.load(file)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+
+        categories = load_from_json('categories')
+        ProductCategory.objects.all().delete()
+        for category in categories:
+            ProductCategory(**category).save()
+
+        products = load_from_json('products')
+        Product.objects.all().delete()
+        for product in products:
+            category_name = product['category']
+            product['category'] = ProductCategory.objects.get(name=category_name)
+            Product(**product).save()
+
+        User.objects.create_superuser(username='admin',password='admin')
